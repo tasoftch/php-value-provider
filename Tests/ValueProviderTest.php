68a4373121c1b11b5b97fa6f5a22eb5cfeb77b44
Thomas Abplanalp <info@tasoft.ch>
2020-04-19 19:07:19 +0200

    Initial Commit

---
+++ Tests/ValueProviderTest.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Copyright (c) 2020 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * ValueProviderTest.php
+ * php-value-storage
+ *
+ * Created on 19.04.20 16:58 by thomas
+ */
+
+namespace TASoft\Util\Test;
+
+use PHPUnit\Framework\TestCase;
+use TASoft\Util\Test\Mock\MockValue;
+use TASoft\Util\Test\Mock\MockValues;
+use TASoft\Util\ValueProvider;
+
+class ValueProviderTest extends TestCase
+{
+	public function testEmptyProvider() {
+		$p = new ValueProvider();
+
+		$this->assertCount(0, $p);
+	}
+
+	public function testScalarValues() {
+		$p = new ValueProvider();
+		$p->test = 23;
+		$p["other-test"] = "Hello World";
+
+		$this->assertCount(2, $p);
+		$this->assertSame(23, $p["test"]);
+		$this->assertSame("Hello World", $p->getValue("other-test"));
+
+		$p["test"] = 44;
+		$this->assertCount(2, $p);
+		$this->assertSame(44, $p->test);
+	}
+
+	public function testValuesConstructor() {
+		$p = new ValueProvider([
+			'test' => 88,
+			'other-test' => 'Hello World'
+		]);
+
+		$this->assertCount(2, $p);
+		$this->assertSame(88, $p["test"]);
+		$this->assertSame("Hello World", $p->getValue("other-test"));
+	}
+
+	public function testExistingValues() {
+		$p = new ValueProvider([
+			'test' => 88,
+			'other-test' => 'Hello World'
+		]);
+
+		$this->assertFalse(isset($p["my-value"]));
+		$this->assertTrue(isset($p["other-test"]));
+	}
+
+	public function testRemovingValues() {
+		$p = new ValueProvider([
+			'test' => 88,
+			'other-test' => 'Hello World'
+		]);
+
+		$p->removeValue("test");
+		$this->assertCount(1, $p);
+		$this->assertNull($p["test"]);
+		$this->assertSame("Hello World", $p->getValue("other-test"));
+
+		unset($p["my-test"]);
+		$this->assertCount(1, $p);
+
+		unset($p["other-test"]);
+		$this->assertCount(0, $p);
+	}
+
+	public function testCallableStorage() {
+		$p = new ValueProvider();
+		$func = function() { return 22; };
+
+		$p->test = $func;
+		$this->assertSame($func, $p->test);
+		$this->assertSame(22, $p["test"]);
+
+		$p = new ValueProvider([
+			'test' => $func
+		]);
+
+		$this->assertSame($func, $p->test);
+		$this->assertSame(22, $p["test"]);
+
+		$p = new ValueProvider();
+		$p["test"] = $func;
+
+		$this->assertSame($func, $p->test);
+		$this->assertSame(22, $p["test"]);
+	}
+
+	public function testValueObjectStorage() {
+		$p = new ValueProvider();
+		$value = new MockValue(22);
+
+		$p->test = $value;
+		$this->assertSame($value, $p->test);
+		$this->assertSame(22, $p["test"]);
+
+		$p = new ValueProvider([
+			'test' => $value
+		]);
+
+		$this->assertSame($value, $p->test);
+		$this->assertSame(22, $p["test"]);
+
+		$p = new ValueProvider();
+		$p["test"] = $value;
+
+		$this->assertSame($value, $p->test);
+		$this->assertSame(22, $p["test"]);
+	}
+
+	public function testValuesObjectStorage() {
+		$p = new ValueProvider();
+		$values = new MockValues();
+
+		$values->values = [
+			'test' => 22,
+			'other-test' => 88
+		];
+
+		$p->test = $values;
+		$this->assertSame(22, $p->test);
+		$this->assertSame(88, $p["other-test"]);
+
+		$p = new ValueProvider([
+			$values
+		]);
+
+		$this->assertSame(22, $p->test);
+		$this->assertSame(88, $p["other-test"]);
+
+		$p = new ValueProvider();
+		$p["test"] = $values;
+
+		$this->assertSame(22, $p->test);
+		$this->assertSame(88, $p["other-test"]);
+	}
+}
---
+++ src/ValueProvider.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Copyright (c) 2020 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace TASoft\Util;
+
+
+use ArrayAccess;
+use Countable;
+use TASoft\Util\Value\ValueInterface;
+use TASoft\Util\Value\ValuesInterface;
+
+class ValueProvider implements ArrayAccess, Countable
+{
+	private $values = [];
+
+	/**
+	 * ValueProvider constructor.
+	 * @param array|iterable $values
+	 */
+	public function __construct(Iterable $values = [])
+	{
+		foreach($values as $key => $value)
+			$this->addValue($key, $value);
+	}
+
+	/**
+	 * @param string $key
+	 * @param $value
+	 * @return static
+	 */
+	public function addValue(string $key, $value) {
+		if(NULL !== $value) {
+			if($value instanceof ValuesInterface) {
+				foreach($value->getValues() as $k => $v)
+					$this->addValue($k, $v);
+			} else
+				$this->values[$key] = $value;
+		}
+		return $this;
+	}
+
+	/**
+	 * @param string $key
+	 */
+	public function removeValue(string $key) {
+		if(isset($this->values[$key]))
+			unset($this->values[$key]);
+	}
+
+	/**
+	 * Gets a value from storage
+	 *
+	 * @param string $key
+	 * @return mixed|null
+	 */
+	public function getValue(string $key) {
+		$v = $this->values[$key] ?? NULL;
+
+		if($v && is_callable($v))
+			$v = call_user_func($v);
+
+		if($v instanceof ValueInterface)
+			$v = $v->getValue();
+
+		return $v;
+	}
+
+	/**
+	 * Gets the initial value from the storage
+	 *
+	 * @param string $key
+	 */
+	public function get(string $key) {
+		return $this->values[$key] ?? NULL;
+	}
+
+	/**
+	 * @param string $key
+	 * @return bool
+	 */
+	public function hasValue(string $key): bool {
+		return isset($this->values[$key]);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __get($name)
+	{
+		return $this->get($name);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __set($name, $value)
+	{
+		$this->addValue($name, $value);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetExists($offset)
+	{
+		return $this->hasValue($offset);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetGet($offset)
+	{
+		return $this->getValue($offset);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->addValue($offset, $value);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetUnset($offset)
+	{
+		$this->removeValue($offset);
+	}
+
+	public function count()
+	{
+		return count($this->values);
+	}
+}
